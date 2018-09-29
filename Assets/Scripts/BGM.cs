286dd32c98434ff14ad4236b804124f818229652
Steven <steven.k.rhodes6@gmail.com>
2018-09-29 01:02:50 -0400

    Adjusted Project Settings & Added BGM Script from BlockBuilder.

---
+++ Assets/Scripts/BGM.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGM : MonoBehaviour
+{
+
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+
+
+
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+
+    void Update()
+    {
+        Scene sceneloadf = SceneManager.GetActiveScene();
+        //Debug.Log("scenenumber " + sceneloadf.buildIndex);
+        if (sceneloadf.buildIndex == 1 || sceneloadf.buildIndex == 2 || sceneloadf.buildIndex == 3 || sceneloadf.buildIndex == 5)
+        {
+            Destroy(this.gameObject);
+        }
+        else if (sceneloadf.buildIndex == 4)
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
