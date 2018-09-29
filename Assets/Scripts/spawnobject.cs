8fcebaf8e34c7bd280adc37e7993cd446b9bbf48
mcma95 <37586569+mcma95@users.noreply.github.com>
2018-09-29 16:45:43 -0400

    adding projectile spawning randomly and block row in the middle of the playground

---
+++ Assets/Scripts/spawnobject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnobject : MonoBehaviour {
+
+    public Vector3 center;
+    public Vector3 size;
+    public GameObject ballprefab;
+
+	// Use this for initialization
+	void Start () {
+        spawnball();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void spawnball()
+    {
+        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
+        Instantiate(ballprefab, pos, Quaternion.identity);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(center, size);
+    }
+}
