a7a605c835f151ed78d4b41fe7659e0542fbcb0b
mcma95 <37586569+mcma95@users.noreply.github.com>
2018-09-29 14:32:30 -0400

    player controller added

---
+++ Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Controller : MonoBehaviour {
+
+
+    public float moveSpeed = 5f;
+    // Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime, 0f, moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);
+        DoAttack();
+	}
+
+    void DoAttack()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Player2Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Controller : MonoBehaviour {
+
+    public float moveSpeed = 5f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal2") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical2") * Time.deltaTime);
+        DoAttack();
+    }
+
+    void DoAttack()
+    {
+        if (Input.GetKey(KeyCode.M))
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.blue;
+        }
+
+    }
+}
