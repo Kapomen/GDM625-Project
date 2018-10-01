ab7d62c02a4d9d1619b240b273d5e847e7a23cff
wimpynannas <eric.wimpee4@gmail.com>
2018-10-01 02:27:10 -0400

    Added Main Menu, Credits Menu, and Options Menu. Main Menu contains buttons that load credits menu, options menu, sample_scene, or exit game. Added sceneManager and quitButton scripts for scene management.

---
+++ Assets/Scripts/SceneTrans.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+//using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneTrans : MonoBehaviour
+{
+
+    public void ChangeToScene(string sceneToChangeTo)
+    {
+        SceneManager.LoadScene(sceneToChangeTo);
+    } //end ChangeToScene
+} //end SceneTrans
