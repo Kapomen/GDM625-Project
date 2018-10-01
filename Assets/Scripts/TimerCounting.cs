ad39be18c9025f2920579e32cab2424a6f704abd
mcma95 <37586569+mcma95@users.noreply.github.com>
2018-09-30 21:41:19 -0400

    add battle timer

---
+++ Assets/Scripts/TimerCounting.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerCounting : MonoBehaviour
+{
+
+    public Text timerText;
+
+    public float startTime;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Time.timeScale = 1;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTimer();
+    }
+
+    void UpdateTimer()
+    {
+        /*
+        GameObject scenepause = GameObject.Find("SceneManager");
+
+        gamepausechecking ifpaused = scenepause.GetComponent<gamepausechecking>();
+        */
+
+        startTime -= Time.deltaTime;
+
+
+        string minutes = Mathf.Floor(startTime / 60).ToString("00");
+        string seconds = Mathf.Floor(startTime % 60).ToString("00");
+
+        timerText.text = minutes + ":" + seconds;
+    }
+}
