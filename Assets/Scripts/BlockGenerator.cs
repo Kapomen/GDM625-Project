6afe4cb6bdbfe2d1b670a2695cdc66b0a6ddf64f
Steven <steven.k.rhodes6@gmail.com>
2018-10-06 15:37:10 -0400

    Setup placeholder crate for player 1 and 2 (Player Crests). Crests spawn across the top row of each player's wall.

--- Assets/Scripts/BlockGenerator.cs
+++ Assets/Scripts/BlockGenerator.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class BlockGenerator: MonoBehaviour
+public class BlockGenerator : MonoBehaviour
 {
+    private readonly List<GameObject> blockTypes = new List<GameObject>();
+    public GameObject WoodCrate;
+    public GameObject PlayerCrest;
 
-    public GameObject block1;
+    private bool playerCrestSet = false; //Set to true for only 1 Crest Block per Player
+    //SpriteRenderer crestSprite;
 
     public int wallWidth = 10;
     public int wallHeight = 10;
@@ -18,6 +23,7 @@ public class BlockGenerator: MonoBehaviour
 
     IEnumerator CreateWall()
     {
+
         for (int x = 0; x < wallWidth; x++)
         {
             yield return new WaitForSeconds(spawnSpeed);
@@ -26,13 +32,35 @@ public class BlockGenerator: MonoBehaviour
             {
                 yield return new WaitForSeconds(spawnSpeed);
 
-                GameObject block = Instantiate(block1, Vector3.zero, block1.transform.rotation) as GameObject;
-                //block.transform.parent = transform;
-                block.transform.SetParent(transform);
-                block.transform.localPosition = new Vector3(x, y, 0);
-            }
-        }
+                if (y == (wallHeight - 1) && playerCrestSet == false)
+                {
+                    SetPlayerCrest(x, y);
+                    print("SetCrest");
+                }
+                else
+                {
+                    SetCrate(x, y);
+                }
+            } //end for loop y
+        } //end for loop x
     }  //end CreateWall
+
+    private void SetCrate(int x, int y) {
+        GameObject block = Instantiate(WoodCrate, Vector3.zero, WoodCrate.transform.rotation) as GameObject;
+        //block.transform.parent = transform;
+        block.transform.SetParent(transform);
+        block.transform.localPosition = new Vector3(x, y, 0);
+
+        //print("SetCrate");
+    } //end SetCrate
+
+    private void SetPlayerCrest(int x, int y)
+    {
+        GameObject block = Instantiate(PlayerCrest, Vector3.zero, PlayerCrest.transform.rotation) as GameObject;
+        //block.transform.parent = transform;
+        block.transform.SetParent(transform);
+        block.transform.localPosition = new Vector3(x, y, 0);
+    } //end SetCrate
 
     //References
     //https://answers.unity.com/questions/490542/spawning-a-grid-of-cubes.html
