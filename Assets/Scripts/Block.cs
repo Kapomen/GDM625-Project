b5ca28fb2ad754281051a8b53b478b0fbb007652
Steven <steven.k.rhodes6@gmail.com>
2018-10-07 19:27:35 -0400

    Updated Block script to now spawn elements when destroyed based on an 'Element Drop Rate' (Public variable set by us in the Unity Inspector).

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -8,10 +8,17 @@ public class Block : MonoBehaviour {
 
     public float ElementDropRate;
 
-	// Use this for initialization
-	void Start () {
+    private GameObject elementSpawner;
+
+    // Use this for initialization
+    void Start () {
         CheckPlayer();
-	}
+        if (isPlayer1Block) {
+            elementSpawner = GameObject.Find("ElementSpawnArea (P1)"); 
+        } else {
+            elementSpawner = GameObject.Find("ElementSpawnArea (P2)");
+        }
+    } //end Start
 	
 	// Update is called once per frame
 	void Update () {
@@ -33,14 +40,15 @@ public class Block : MonoBehaviour {
             //Destroy(this.gameObject);
         }
         if (col.gameObject.tag == "Ball")
-        {   
+        {
             Destroy(this.gameObject);
             GameManager.Instance.BlockDestroyed(isPlayer1Block);
 
             float elementDrop = Random.Range(0.0f, 1.0f);
             if(elementDrop <= ElementDropRate)
             {
-                //SpawnElement();
+                ElementGenerator elementGenerator = elementSpawner.GetComponent<ElementGenerator>();
+                elementGenerator.SpawnElement();
                 print("Element Dropped (" +elementDrop + "%)");
             }
         }
