e6f8ba43f7eac8fc1febb856aca1001de8647002
wimpynannas <eric.wimpee4@gmail.com>
2018-10-14 01:19:27 -0400

    Imported ButtonSelection sound and set up audio controller for playback on Main Menu..

---
+++ Assets/Scripts/SelectionSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class SelectionSound : MonoBehaviour {
+
+    public AudioClip sound;
+    private Button button { get { return GetComponent<Button>(); } }
+    private AudioSource source {get { return GetComponent<AudioSource>(); } }
+
+	// Use this for initialization
+	void Start () {
+        gameObject.AddComponent<AudioSource>();
+        source.clip = sound;
+        source.playOnAwake = false;
+        button.onClick.AddListener(() => playSound());
+	}
+	
+	void playSound () {
+        source.PlayOneShot(sound);
+	}
+}
