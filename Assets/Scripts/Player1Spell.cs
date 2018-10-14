d58d1b131c06a4a23d50cbcec2ff1559a682a8b9
Sicheng Ma <masicheng1995@gmail.com>
2018-10-14 12:54:23 -0400

    adding player spell manager to check if two water pickup then spawn ice floor

---
+++ Assets/Scripts/Player1Spell.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Spell : MonoBehaviour {
+    public int pickupwater;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject icefloorp2 = GameObject.Find("IceFloorSpawnArea (P2)");
+
+        IceFloorGenerator player1spawn = icefloorp2.GetComponent<IceFloorGenerator>();
+
+        if (pickupwater == 2)
+        {
+            player1spawn.SpawnIceFloor();
+            pickupwater = 0;
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "waterpickup")
+        {
+            pickupwater++;
+        }
+    }
+}
---
+++ Assets/Scripts/Player2Spell.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Spell : MonoBehaviour {
+
+    public int pickupwater;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject icefloorp1 = GameObject.Find("IceFloorSpawnArea (P1)");
+
+        IceFloorGenerator player2spawn = icefloorp1.GetComponent<IceFloorGenerator>();
+
+        if (pickupwater == 2)
+        {
+            player2spawn.SpawnIceFloor();
+            pickupwater = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "waterpickup")
+        {
+            pickupwater++;
+        }
+    }
+}
