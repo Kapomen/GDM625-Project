d899a5910824e9747aa997fd2eece28930221dbf
mcma95 <37586569+mcma95@users.noreply.github.com>
2018-10-14 23:19:31 -0400

    fix ice floor bugs, add stage music, make ball not move when battle ends

--- Assets/Scripts/AudienceMovement.cs
+++ Assets/Scripts/AudienceMovement.cs
@@ -4,18 +4,48 @@ using UnityEngine;
 
 public class AudienceMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float delta = 0.1f;  // Amount to move left and right from the start point
+    public float speed = 10f;
+    private Vector3 startPos;
+    public bool leftright;
+    public bool updown;
 
-    void AudienceMove()
+
+    void Start()
     {
+        startPos = transform.position;
+    }
 
+    void Update()
+    {
+        GameObject checkifscore = GameObject.Find("Ball");
+
+        Projectile isscore = checkifscore.GetComponent<Projectile>();
+        
+        if (leftright && isscore.score)
+        {
+            leftandright();
+            updown = false;
+        }
+        if (updown && isscore.score)
+        {
+            
+            upanddown();
+            leftright = false;
+        }
+    }
+
+    void leftandright()
+    {
+        Vector3 v = startPos;
+        v.x += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+
+    void upanddown()
+    {
+        Vector3 v = startPos;
+        v.y += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
     }
 }
